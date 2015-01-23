613640b0579fad2c0df43566c978c29f5269a314
Bob Fitterman <bob@eachscape.com>
2015-01-23 17:04:35 -0500

    And you need to compile the assets.

--- server/config/initializers/assets.rb
+++ server/config/initializers/assets.rb
@@ -6,3 +6,5 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+Rails.application.config.assets.precompile += %w{ jquery.ba-throttle-debounce.min-1.1.js underscore-min-1.7.0.js backbone-min-1.1.2.js}
