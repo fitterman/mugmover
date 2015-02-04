dec596968f353c801b2221c42bd6891c7d3f3e9b
Bob Fitterman <bob@eachscape.com>
2015-02-04 06:56:57 -0500

    Nice name picking!

--- server/app/models/named_face.rb
+++ server/app/models/named_face.rb
@@ -3,9 +3,14 @@ class NamedFace < ActiveRecord::Base
   belongs_to  :hosting_service_account
   has_many    :faces
 
+  default_scope { order('public_name') } 
+
   validates   :face_key,               presence: {allow_blank: false}
   validates   :face_name_uuid,         presence: {allow_blank: false}
   validates   :private_name,           presence: {allow_blank: false}
   validates   :public_name,            presence: {allow_blank: false}
 
+  def png_thumbnail
+    Base64.encode64(open('/Users/bob/Downloads/logo-200x200.png') { |io| io.read })
+  end
 end
