431bd8b1ab95faf1569b73c48cfb621a0706762c
Bob Fitterman <bob@eachscape.com>
2015-03-24 09:17:09 -0400

    Along the way I had to update some pods, but others are still in flux so the podfile is not commited here.

---
+++ osx/mugmover/MMSmugmugOauth.h
@@ -0,0 +1,13 @@
+//
+//  MMSMugmugOauth.h
+//  mugmover
+//
+//  Created by Bob Fitterman on 3/24/15.
+//  Copyright (c) 2015 Dicentra LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MMSMugmugOauth : NSObject
+
+@end
---
+++ osx/mugmover/MMSmugmugOauth.m
@@ -0,0 +1,13 @@
+//
+//  MMSMugmugOauth.m
+//  mugmover
+//
+//  Created by Bob Fitterman on 3/24/15.
+//  Copyright (c) 2015 Dicentra LLC. All rights reserved.
+//
+
+#import "MMSMugmugOauth.h"
+
+@implementation MMSMugmugOauth
+
+@end
