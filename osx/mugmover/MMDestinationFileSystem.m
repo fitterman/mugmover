2c9614542088ebc76d9b651649a61e373f22fa94
Bob Fitterman <bobf@jhu.edu>
2018-07-02 15:35:00 -0400

    Strip out a lot of wishful code (base on the Smugmug capabilities) and get the exiftool caption formatter going.

--- osx/mugmover/MMDestinationFileSystem.m
+++ osx/mugmover/MMDestinationFileSystem.m
@@ -217,51 +217,13 @@ NSString *destTypeIdentifier = @"filesystem";
         NSArray *photos = [MMPhoto getPhotosForEvent: event];
 
         // For file transfers, we generate a script that can be used to update the Exif
-        // data after the export. It's an array of strings, one per file.
-        // We have an extra so we can get a terminal newline in the file.
-        NSMutableArray *scriptCommands = [[NSMutableArray alloc] initWithCapacity: [photos count] + 1];
+        // data after the export. It's an array of strings, two per file. (one for echo,
+        // one for exiftool). There's an extra so we can get a terminal newline in the file.
+        NSMutableArray *scriptCommands = [[NSMutableArray alloc] initWithCapacity: (2 * [photos count]) + 1];
 
         // Get the preferences (defaults) for this event within this service
         NSMutableDictionary *albumState = [[defaults objectForKey: albumKey] mutableCopy];
-        NSString *albumId = nil;
-/*
-        if (albumState)
-        {
-            albumId = [albumState objectForKey: @"albumId"];
-            if (albumId)
-            {
-                // If you can't find the album, clear the "albumId" so a new one will be created
-                if (![self hasAlbumId: albumId])
-                {
-                    albumId = nil;
-                }
-            }
-        }
-        else // albumState has never been saved, initialize it...
-        {
-            albumState = [[NSMutableDictionary alloc] init];
-        }
-*/
-        // If the albumId is present and not found or this is a new (to us) album,
-        // go ahead and create one.
-/*
-        BOOL albumCreatedOnThisPass = NO;
-        if (!albumId)
-        {
-            NSString *description = [NSString stringWithFormat: @"From event \"%@\", uploaded via Mugmover", name];
-            // If the old AlbumID hasn't been stored or can't be found, create a new one
-            albumId = [self createAlbumWithUrlName: [MMDestinationFileSystem sanitizeUuid: [event uuid]]
-                                          inFolder: folderId
-                                       displayName: name
-                                       description: description];
-            albumCreatedOnThisPass = (albumId != nil);
-            [albumState setValue: albumId forKey: @"albumId"];
-            NSMutableDictionary *mappingDictionary = [[NSMutableDictionary alloc] initWithCapacity: [photos count]];
-            [albumState setValue: mappingDictionary forKey: @"mapping"];
-        }
-        
-        // We use these next two to keep track of whether everything completes
-*/
+
         NSInteger completedTransfers = 0;
         NSInteger allCounter = 0;
         MMEventStatus finalStatus = MMEventStatusIncomplete; // Assume something goes wrong
@@ -275,23 +237,6 @@ NSString *destTypeIdentifier = @"filesystem";
             if (uploadOperation.isCancelled)
             {
                 break;
-            }
-            
-            // The +mappingKeyPath+ points to the Smugmug photo ID associated with this photo the
-            // last time it was uploaded (if ever). This information can be used to facilitate the
-            // replacement of the image. For now, it's just a way to know not to repeat an upload.
-            NSString *mappingKeyPath = [NSString stringWithFormat: @"mapping.%@", photo.versionUuid];
-            NSString *replacementFor = nil;
-            if (mappingKeyPath)
-            {
-                replacementFor = [albumState valueForKeyPath: mappingKeyPath];
-                // If it's already marked as uploaded and we aren't reprocessing, skip it...
-                if (replacementFor && !reprocessAllImagesPreviouslyTransmitted)
-                {
-                    completedTransfers++;   // We consider it sent already so we can get the icons right
-                    [windowController incrementProgressBy: 1.0];
-                    continue;               // And then we skip the processing
-                }
             }
             
             error = [photo processPhoto];
@@ -314,33 +259,6 @@ NSString *destTypeIdentifier = @"filesystem";
                      }
                  ];
             }
-/*
-            __block NSString *smugmugImageId = nil;
-            // This must be declared inside the loop because it references "photo"
-            ServiceResponseHandler processSmugmugUpload = ^(NSDictionary *response)
-            {
-                if ([[response valueForKeyPath: @"stat"] isEqualToString: @"ok"])
-                {
-                    NSMutableDictionary *serviceDictionary = [[NSMutableDictionary alloc] init];
-                    [serviceDictionary setObject: @"smugmug"
-                                          forKey: @"name"];
-                    [serviceDictionary setObject: [response valueForKeyPath: @"Image.URL"]
-                                          forKey: @"url"];
-                    [serviceDictionary setObject: _handle
-                                          forKey: @"handle"];
-                    [photo attachServiceDictionary: serviceDictionary];
-                    
-                    NSString *imageUri = [response valueForKeyPath: @"Image.ImageUri"];
-                    NSArray *pieces = [[imageUri lastPathComponent] componentsSeparatedByString: @"-"];
-                    smugmugImageId = pieces[0];
-                }
-                else // In theory, you will no longer get here because we check for errors
-                {
-                    DDLogError(@"response=%@", response);
-                }
-            };
-
- */
             NSString *copiedFilePath = nil;
             
             BOOL imageRequiresConversion = [photo isFormatRequiringConversion];
@@ -364,85 +282,20 @@ NSString *destTypeIdentifier = @"filesystem";
                 break; // TODO Verify we want to break out of the loop.
             }
             
-            NSString *description = @"";
-            if (photo.caption != nil)
-            {
-                description = [description stringByAppendingString: photo.caption];
-            }
-            NSString* cmd = [NSString stringWithFormat: @"exiftool '%@' -DateTimeOriginal='%@' -Description '%@'",
+            NSString* cmd = [NSString stringWithFormat: @"echo '%@'", copiedFilePath];
+            [scriptCommands addObject: cmd];
+            cmd = [NSString stringWithFormat: @"exiftool '%@' -DateTimeOriginal='%@' -Description=%@",
                                 copiedFilePath,
                                 [photo.originalDate stringByReplacingOccurrencesOfString: @"-" withString: @":"],
-                                description];
+                                [MMFileUtility bashEscapedString: [photo formattedDescription]]];
             [scriptCommands addObject: cmd];
-            /*
-            if (!uploadRequest)
-            {
-                // No uploadRequest and no error indicates we have been asked to
-                // replace a file that already has been updated.
-                
-                // TODO We need to still update the object on Mugmarker, being care to update
-                //      not create nor to fully replace the object (as you could have data collisions).
-                smugmugImageId = replacementFor;
-            }
-            else
-            {
-                // 1. Upload to Smugmug
-                error = [_smugmugOauth synchronousUrlRequest: uploadRequest
-                                                       photo: photo
-                                           remainingAttempts: MMDefaultRetries
-                                           completionHandler: processSmugmugUpload];
-                if (imageRequiresConversion) // There's some cleanup to do before checking for an error
-                {
-                    // Delete the temp directory
-                    [[NSFileManager defaultManager] removeItemAtPath: pathToFileToUpload
-                                                               error: nil];
-                }
-                if (error)
-                {
-                    [self logError: error];
-                    DDLogError(@"Upload to Smugmug server failed for photo %@.", photo);
-                    continue;
-                }
-                
-                // 2. Get the image sizes and update to the photo object
-                NSDictionary *sizes = [self imageSizesForPhotoId: smugmugImageId];
-                [photo setUrlsForLargeImage: [sizes valueForKeyPath: @"ImageSizeLarge.Url"]
-                              originalImage: [sizes valueForKeyPath: @"ImageSizeOriginal.Url"]];
-                
-                // 3. Upload the data to Mugmarker
-                error = [photo sendPhotoToMugmover];
-                if (error)
-                {
-                    [self logError: error];
-                    DDLogError(@"Upload to MM server failed for photo %@, error %@.", photo, error);
-                    
-                    // Because this was a new image (not a replacement to Smugmug),
-                    // we attempt to delete the photo from the service so it's not
-                    // on the service and missing from Mugmarker.
-                    if (![self deletePhotoId: smugmugImageId])
-                    {
-                        DDLogError(@"Cleanup deletion failed for photo %@", smugmugImageId);
-                    }
-                    else
-                    {
-                        smugmugImageId = nil; // So we think it failed (because it did)
-                    }
-                    continue;
-                }
-            }
-            // 4. Now we mark it as sent in our local store
-            if (smugmugImageId)
-            {
-                // We do it on each pass in case it bombs on a large collection of photos
-                [albumState setValue: smugmugImageId forKey: mappingKeyPath];
-                [defaults setObject: albumState forKey: albumKey];
-                [MMPrefsManager syncIfNecessary: defaults];
-                completedTransfers++;
-                [windowController incrementProgressBy: 1.0];
-            }
- */
+
+            completedTransfers++;
+            [windowController incrementProgressBy: 1.0];
         }
         
+        finalStatus = (completedTransfers == [photos count]) ? MMEventStatusCompleted : MMEventStatusIncomplete;
+
         // Create the exiftool command file
         
         // First we concatenate the strings
@@ -455,64 +308,21 @@ NSString *destTypeIdentifier = @"filesystem";
         
         [[NSFileManager defaultManager] createFileAtPath: exiftoolFile
                                                     contents: nil
-                                                  attributes: nil];
+                                                  attributes: @{ NSFilePosixPermissions : @0544 }];
+        
         [exiftoolCommands writeToFile: exiftoolFile
                            atomically: YES
                              encoding: NSUTF8StringEncoding
                                 error: &writeError];
         if (writeError != nil)
         {
-            // TODO Do something
+            finalStatus = MMEventStatusIncomplete;    // TODO Do something
         }
         
-        // The finalStatus should reflect whether a writeError occurred or not
-        
-        finalStatus = (completedTransfers == [photos count]) ? MMEventStatusCompleted : MMEventStatusIncomplete;
 
         // And at the end we have to do it in case some change(s) did not get stored
         [defaults setObject: albumState forKey: albumKey];
-        [MMPrefsManager syncIfNecessary: defaults];
-/*        
-        // If we were unable to do any transfers AND this is a newly-created album, we need to
-        // delete the album as there is no record of its existence preserved and it will just hang
-        // out in an unusable state.
-        if (albumCreatedOnThisPass && (completedTransfers == 0))
-        {
-            if (![self deleteAlbumId: albumId])
-            {
-                DDLogError(@"Cleanup deletion failed for album %@", albumId);
-            }
-        }
-        else
-        {
-            // Now we update the featured photo for the album
-            NSString *featuredPhotoUuid = [event featuredImageUuid];
-            NSString *featuredPhotoMappingPath = [NSString stringWithFormat: @"mapping.%@", featuredPhotoUuid];
-            if (featuredPhotoUuid && featuredPhotoMappingPath)
-            {
-                NSString *featuredPhotoId = [albumState valueForKeyPath: featuredPhotoMappingPath];
-                if (featuredPhotoId)
-                {
-                    NSString *featuredImageUri = [NSString stringWithFormat: @"/api/v2/album/%@/image/%@",
-                                                  albumId,
-                                                  featuredPhotoId];
-                    NSString *apiCall = [NSString stringWithFormat: @"album/%@", albumId];
-                    NSURLRequest *eventRequest = [_smugmugOauth apiRequest: apiCall
-                                                                parameters: @{@"HighlightAlbumImageUri": featuredImageUri}
-                                                                      verb: @"PATCH"];
-                    error = [_smugmugOauth synchronousUrlRequest: eventRequest
-                                                           photo: nil
-                                               remainingAttempts: MMDefaultRetries
-                                               completionHandler: nil];
-                    if (error)
-                    {
-                        // TODO Log this error!
-                        DDLogWarn(@"Unable to set featured image for event album (%@).", event.name);
-                    }
-                }
-            }
-        }
- */
+
         // Restore the display to the default image for this album
         [event setActivePhotoThumbnail: nil withStatus: finalStatus];
         [[NSOperationQueue mainQueue] addOperationWithBlock: ^(void)
